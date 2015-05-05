17e930b2a50b9d78c89e7c9c39c2ca54c9ceb4b7
ssgrn <sgrangan01@gmail.com>
2015-05-05 14:06:11 -0400

    First iteration of networking complete.
    Time to playtest

--- Car Game/Assets/Custom Assets/Network/NetworkManager.cs
+++ Car Game/Assets/Custom Assets/Network/NetworkManager.cs
@@ -2,14 +2,31 @@
 using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
-
+	const string VERSION = "v1.0";
+	public string roomName = "CarGame";
+	public string playerPrefabName = "Gun Car";
+	public Transform spawnPoint;
+	
 	// Use this for initialization
 	void Start () {
-	
+		PhotonNetwork.ConnectUsingSettings(VERSION);
+		
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnJoinedLobby(){
+		RoomOptions roomOptions = new RoomOptions () { isVisible = false, maxPlayers = 4 };
+		PhotonNetwork.JoinOrCreateRoom (roomName, roomOptions, TypedLobby.Default);
 	}
+	
+	void OnJoinedRoom(){
+		PhotonNetwork.Instantiate (playerPrefabName, 
+		                           spawnPoint.position,
+		                           spawnPoint.rotation,
+		                           0);
+		
+	}
+	
+	
+	
 }
--- Car Game/Assets/Custom Assets/Network/NetworkPlayer.cs
+++ Car Game/Assets/Custom Assets/Network/NetworkPlayer.cs
@@ -5,11 +5,9 @@ public class NetworkPlayer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
